eb4fcc8f1402af664ae275be9ccdff31ebd143d5
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-05-02 12:54:07 -0300

    Parte_1

---
+++ IA_Comportamentos_E_Movimentações/Assets/FlockingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    public int numFish= 20;
+    public GameObject[] allFish; 
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x), 
+                                                                Random.Range(-swinLimits.y, swinLimits.y), 
+                                                                Random.Range(-swinLimits.z, swinLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
